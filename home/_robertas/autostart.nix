bcd4c20e7b96e5373316e6dce64f9aeea21d0fa7
RobertasJ <robertasj8@gmail.com>
2025-05-17 21:18:22 +0200

    make vesktop autostart

--- home/_robertas/autostart.nix
+++ home/_robertas/autostart.nix
@@ -5,6 +5,7 @@
     entries = [
       "${pkgs.kdePackages.yakuake}/share/applications/org.kde.yakuake.desktop"
       "${pkgs.megasync}/share/applications/megasync.desktop"
+      "${pkgs.vesktop}/share/applications/vesktop.desktop"
     ];
   };
 }
